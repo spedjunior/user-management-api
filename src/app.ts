39573593634379957e785f134476a055a39b8d72
spedjunior <spedjunior@gmail.com>
2024-04-10 20:48:43 -0300

    feat: add initial openapi settings

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@
 
 import express from 'express';
 import { router } from './routes';
+import swaggerUi  from 'swagger-ui-express';
+import swaggerFile from '../openapi.json';
 //import promBundle from 'express-prom-bundle';
 //import newrelic from 'newrelic';
 
@@ -21,6 +23,7 @@ const app = express();
 
 //app.use(metricsMiddleware);
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router)
 
 export { app }
