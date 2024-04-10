7ed6d91d0df50bc7d9193b27563953e9b229856f
spedjunior <spedjunior@gmail.com>
2024-04-09 22:55:35 -0300

    feat: add opentelemetry

---
+++ src/instrumentation.ts
@@ -0,0 +1,28 @@
+/* instrumentation.ts */
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+/*import {
+   PeriodicExportingMetricReader,
+   //  ConsoleMetricExporter,
+} from '@opentelemetry/sdk-metrics'; */
+
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+
+const sdk = new NodeSDK({
+   traceExporter: new ConsoleSpanExporter(),
+   metricReader: new PrometheusExporter({ port: 9464 }),
+   instrumentations: [getNodeAutoInstrumentations(), new HttpInstrumentation(), new ExpressInstrumentation()],
+});
+
+//const sdk = new NodeSDK({
+//  traceExporter: new ConsoleSpanExporter(),
+//  metricReader: new PeriodicExportingMetricReader({
+//    exporter: new ConsoleMetricExporter(),
+//  }),
+//  instrumentations: [getNodeAutoInstrumentations()],
+//});
+
+sdk.start();
